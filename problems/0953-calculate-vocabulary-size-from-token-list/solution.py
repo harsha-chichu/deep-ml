abd452c8e42376b3d9d95bed639bd3f37668d9a3
harsha-chichu <98161308+harsha-chichu@users.noreply.github.com>
2026-09-22 11:53:58 +0000

    Update: Calculate Vocabulary Size from Token List

--- problems/0953-calculate-vocabulary-size-from-token-list/solution.py
+++ problems/0953-calculate-vocabulary-size-from-token-list/solution.py
@@ -8,16 +8,7 @@ def vocab_size(tokens, special_tokens=None):
         int: number of unique tokens in the combined vocabulary
     """
 
-    if tokens or special_tokens:
-        table = {}
-
-        for i in tokens:
-            table[i] = table.get(i, 0) + 1
-        
-        if special_tokens is not None:
-            for i in special_tokens:
-                table[i] = table.get(i, 0) + 1
-            
-        return len(table)
-
-    return 0
+    if not tokens and not special_tokens:
+        return 0
+    vocab = set(tokens) | set(special_tokens or [])
+    return len(vocab)
